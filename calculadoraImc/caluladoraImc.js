8670ca22e067e96b9640eade67030a6d87a1c615
pruebagithub246 <pruebagithub93@gmail.com>
2024-01-16 20:29:52 +0100

    mejora de diseño y funcionalidad
    se ha mejora algun aspecto de la pagina y añadido una funcionalidad nueva, que dependiendo del imc te dice en que rango estas

--- calculadoraImc/caluladoraImc.js
+++ calculadoraImc/caluladoraImc.js
@@ -2,9 +2,23 @@ let btnCalcularImc=document.getElementById('calculadoraImc');
 btnCalcularImc.addEventListener('click',e=>{
     let altura=document.getElementById('altura').value;
     let peso=document.getElementById('peso').value;
-    document.getElementById('resultado').innerHTML=calcularImc(altura,peso);
+    let imc=calcularImc(altura,peso);
+    document.getElementById('resultado').innerHTML=imc+' tu rango de peso esta en:'+rangoImc(imc);
 });
 
 function calcularImc(altura,peso) {
     return (peso/(altura**2)).toFixed(1);
+}
+
+function rangoImc(imc) {
+    const IMCS=[18.5,24.9,25,29.9,30];
+    if (imc<IMCS[0]) {
+        return 'Peso insuficiente';
+    } else if (imc>IMCS[0] && imc<IMCS[1]) {
+        return 'Peso normal';
+    } else if (imc>IMCS[2] && imc<IMCS[3]) {
+        return 'Peso sobrepeso';
+    }else if (imc>IMCS[4]) {
+        return 'obesidad';
+    }
 }
