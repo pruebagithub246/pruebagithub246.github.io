9995865b6f42b4a898faf2b7b481615fd7c8b524
pruebagithub246 <pruebagithub93@gmail.com>
2024-01-17 20:05:50 +0100

    implementacion de nuevo codigo para añadir productos
    nueva pagina con una funcionalidad de añadir productos a una cesta y tambien poder eliminarla

---
+++ ejercicioCesta/ejercicioCesta.js
@@ -0,0 +1,33 @@
+let cesta=[];
+let btnAnadirProducto=document.getElementById('anadirProducto');
+let btnMostrarCesta=document.getElementById('mostrarCesta');
+
+let idParrafo=1;
+btnAnadirProducto.addEventListener('click',e=>{
+    let producto=document.getElementById('producto').value;
+    cesta.push( {id:idParrafo,nombre:producto});
+   // alert('producto añadido');
+   idParrafo++;
+});
+
+let divCesta=document.getElementById('cesta');
+btnMostrarCesta.addEventListener('click',e=>{
+    mostrarCesta(cesta);
+});
+
+function mostrarCesta(cesta) {
+    divCesta.innerHTML='';
+    let idParrafo=1;
+    let ul=document.createElement('ul');
+    cesta.forEach(producto=>{
+        let li=document.createElement('li');
+        li.textContent=idParrafo+'.'+producto.nombre;
+        li.addEventListener('click',(e)=>{
+            cesta.splice(cesta.indexOf(producto),1);
+            mostrarCesta(cesta);
+        })
+        ul.appendChild(li);
+        idParrafo++;
+    });
+    divCesta.append(ul); 
+}
